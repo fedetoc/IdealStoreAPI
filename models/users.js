298977fe27a26df38b4d24aad9b88238578b9832
Federico Tocco <fedecap26@gmail.com>
2021-10-06 23:41:40 -0300

    eliminado del campo confirmPassword al subir a la base

--- models/users.js
+++ models/users.js
@@ -58,6 +58,7 @@ const encryptPassword = async function (next) {
 	await encrypt(this.password, (err, encrypted) => {
 		if (err) return next(err);
 		this.password = encrypted;
+		this.confirmPassword = null;
 		return next();
 	});
 };
